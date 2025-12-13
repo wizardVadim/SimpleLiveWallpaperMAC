d135687c6ad3c1119fad767e9053098b5ccf6e4e
Vekhov Vadim <106478770+wizardVadim@users.noreply.github.com>
2025-12-13 22:36:47 +0800

    Initial Commit

---
+++ SimpleLiveWallpaperMAC/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  SimpleLiveWallpaperMAC
+//
+//  Created by Вадим Вехов on 13.12.2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ SimpleLiveWallpaperMAC/SimpleLiveWallpaperMACApp.swift
@@ -0,0 +1,17 @@
+//
+//  SimpleLiveWallpaperMACApp.swift
+//  SimpleLiveWallpaperMAC
+//
+//  Created by Вадим Вехов on 13.12.2025.
+//
+
+import SwiftUI
+
+@main
+struct SimpleLiveWallpaperMACApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
